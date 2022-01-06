3eb9e8bf850879cd678a70dff23d146941df224e
Max2 <chigov@gmail.com>
2022-01-06 18:04:31 +0200

    RecyclerView

--- app/src/main/java/com/chigov/retrofit/MainActivity.java
+++ app/src/main/java/com/chigov/retrofit/MainActivity.java
@@ -1,14 +1,39 @@
 package com.chigov.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.chigov.retrofit.adapters.EmployeeAdapter;
+import com.chigov.retrofit.pojo.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewEmployees;
+    private EmployeeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewEmployees = findViewById(R.id.recyclerViewEmployees);
+        adapter = new EmployeeAdapter();
+        recyclerViewEmployees.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewEmployees.setAdapter(adapter);
+
+        List<Employee> employees = new ArrayList<>();
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee();
+        employee1.setfName("Max");
+        employee2.setfName("Ivan");
+        employee1.setlName("Ivanov");
+        employee2.setlName("Petrov");
+        employees.add(employee1);
+        employees.add(employee2);
+        adapter.setEmployees(employees);
     }
 }
